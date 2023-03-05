427855dd2156963e9540605a737e48cb94f656af
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-03-05 20:33:52 -0300

    agrupando-components

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -13,7 +13,6 @@ const Planet = () => {
         class="gray-img"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/900px-Mercury_in_color_-_Prockter07-edit1.jpg"
       ></img>
-      <p>lorem ipsum</p>
     </div>
   );
 };
