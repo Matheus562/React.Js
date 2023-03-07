acfd7d4caa1281ea8fb65d18c0f97703a7e72baa
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-03-07 17:36:44 -0300

    renderização-condicional

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -1,10 +1,6 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
-const clickOnPlanet = (name) => {
-  alert(`Um click no planeta: ${name}`);
-};
-
 const Planets = () => {
   return (
     <Fragment>
@@ -15,14 +11,13 @@ const Planets = () => {
         description="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres."
         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/900px-Mercury_in_color_-_Prockter07-edit1.jpg"
         link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-        clickOnPlanet={clickOnPlanet}
+        title_with_underline={true}
+        gray={true}
       />
       <Planet
         name="Plutão"
         description="Plutão, formalmente designado 134340 Plutão é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol."
         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/420px-Pluto_in_True_Color_-_High-Res.jpg"
-        link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
-        clickOnPlanet={clickOnPlanet}
       />
     </Fragment>
   );
--- src/components/shared/description_with_link/index.js
+++ src/components/shared/description_with_link/index.js
@@ -1,13 +1,27 @@
 import React, { Fragment } from "react";
 
 const DescriptionWithLink = (props) => {
-  return (
-    <p>
-      <fragment>
-        <p>{props.description}</p>
-        <a href={props.link}>{props.link}</a>
-      </fragment>
-    </p>
-  );
+  if (props.link) {
+    return (
+      <p>
+        <fragment>
+          <p>{props.description}</p>
+          <p>
+            <a href={props.link}>{props.link}</a>
+          </p>
+        </fragment>
+      </p>
+    );
+  } else {
+    return (
+      <p>
+        <fragment>
+          <p>
+            <u>{props.description}</u>
+          </p>
+        </fragment>
+      </p>
+    );
+  }
 };
 export default DescriptionWithLink;
