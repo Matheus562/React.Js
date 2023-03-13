99ba7a806ca1189c8542bcb163bf2352d67100aa
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-03-12 21:30:36 -0300

    exercício-criando-um-component-com-props

--- src/components/shared/gray_img/index.js
+++ src/components/shared/gray_img/index.js
@@ -2,6 +2,6 @@ import React, { Fragment } from "react";
 import "../gray_img/style.css";
 
 const GrayImg = (props) => {
-  return <img class="gray-img" src={props.img_url}></img>;
+  return <img className="gray-img" src={props.img_url}></img>;
 };
 export default GrayImg;
