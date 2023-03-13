ea626f7bcd840afd1863f0476cb0a625f16fa943
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-03-12 21:33:40 -0300

    dividindo-e-reutilizando-components

--- src/components/shared/gray_img/index.js
+++ src/components/shared/gray_img/index.js
@@ -4,7 +4,7 @@ import "../gray_img/style.css";
 const GrayImg = () => {
   return (
     <img
-      class="gray-img"
+      className="gray-img"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/900px-Mercury_in_color_-_Prockter07-edit1.jpg"
     ></img>
   );
