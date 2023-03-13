73fd955d65b1c046557b88372367eef916b76045
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-03-12 22:45:32 -0300

    refatorando-lifecycle-com-hooks

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Planet from "./planet";
 
 async function getPlanets() {
@@ -15,17 +15,13 @@ async function getPlanets() {
 }*/
 
 const Planets = () => {
-  const [planets, setPlanets] = useState([
-    {
-      id: "mars",
-      name: "Mars",
-      description:
-        "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.",
-      img_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
-      link: "https://en.wikipedia.org/wiki/Mars",
-    },
-  ]);
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    getPlanets().then((data) => {
+      setPlanets(data["planets"]);
+    });
+  }, []);
+
   const removeLast = () => {
     //... pega todas as proprieadades do objeto
     let new_planets = [...planets];
