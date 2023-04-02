7829615d4da3f337b250b4256a53c8453dcd3013
Matheus Lucas <matheuslucas562@yahoo.com.br>
2023-04-02 11:06:32 -0300

    criando-multiplos-inputs

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from "react";
 import "../../shared/gray_img/style.css";
 import GrayImg from "../../shared/gray_img/index.js";
 import DescriptionWithLink from "../../shared/description_with_link";
+import Form from "./form";
 
 async function getSatellites(id) {
   let response = await fetch(`http://localhost:3000/api/${id}.json`);
@@ -18,6 +19,10 @@ const Planet = (props) => {
     });
   }, []);
 
+  const addSatellite = (new_satellite) => {
+    setSatellites([...satellites, new_satellite]);
+  };
+
   let title;
   if (props.title_with_underline)
     title = (
@@ -33,6 +38,9 @@ const Planet = (props) => {
       <DescriptionWithLink description={props.description} link={props.link} />
       <GrayImg img_url={props.img_url} gray={props.gray} />
       <h4>Satélites</h4>
+      <hr />
+      <Form addSatellite={addSatellite} />
+      <hr />
       <ul>
         {satellites.map((satellite, index) => (
           <li key={index}>{satellite.name}</li>
